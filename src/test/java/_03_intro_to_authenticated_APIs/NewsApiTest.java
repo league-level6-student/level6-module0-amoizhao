b8e9022b91a47956e481c567ac150f8bd6254696
amoizhao <zhaoa6125@gmail.com>
2021-08-21 17:53:30 -0700

    -_-

--- src/test/java/_03_intro_to_authenticated_APIs/NewsApiTest.java
+++ src/test/java/_03_intro_to_authenticated_APIs/NewsApiTest.java
@@ -17,6 +17,7 @@ import _01_intro_to_APIs.data_transfer_objects.Result;
 import reactor.core.publisher.Mono;
 
 import java.net.URI;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.function.Function;
@@ -43,31 +44,80 @@ class NewsApiTest {
     WebClient.ResponseSpec responseSpecMock;
 
     @Mock
-    Mono<Result[]> resultMonoMock;
+    Mono<ApiExampleWrapper> resultMonoMock;
 
     @BeforeEach
     void setUp() {
     	MockitoAnnotations.openMocks(this);
 
     	newsApi = new NewsApi();
+    	newsApi.setWebClient(webClientMock);
     }
 
     @Test
     void itShouldGetNewsStoryByTopic() {
         //given
+    	String topic = "pigeons";
+
+        ApiExampleWrapper expectedWrapper = new ApiExampleWrapper();
+        
+        when(webClientMock.get())
+                .thenReturn(requestHeadersUriSpecMock);
+        when(requestHeadersUriSpecMock.uri((Function<UriBuilder, URI>) any()))
+                .thenReturn(requestHeadersSpecMock);
+        when(requestHeadersSpecMock.retrieve())
+                .thenReturn(responseSpecMock);
+        when(responseSpecMock.bodyToMono(ApiExampleWrapper.class))
+                .thenReturn(resultMonoMock);
+        when(resultMonoMock.block())
+                .thenReturn(expectedWrapper);
 
         //when
+        ApiExampleWrapper actualWrapper = newsApi.getNewsStoryByTopic(topic);
 
         //then
+        verify(webClientMock, times(1)).get();
+        assertEquals(expectedWrapper, actualWrapper);
     }
 
     @Test
     void itShouldFindStory(){
         //given
+    	String topic = "pigeons";
+        ArrayList<Article> newsArticles = new ArrayList<Article>();
+        
+        Article article = new Article();
+        article.setTitle("some pigeons");
+        article.setContent("pigeons are irritating");
+        article.setUrl("www.pigeons.com");
+        
+        newsArticles.add(article);
 
+        ApiExampleWrapper expectedWrapper = new ApiExampleWrapper();
+        expectedWrapper.setArticles(newsArticles);
+
+        when(webClientMock.get())
+        		.thenReturn(requestHeadersUriSpecMock);
+		when(requestHeadersUriSpecMock.uri((Function<UriBuilder, URI>) any()))
+		        .thenReturn(requestHeadersSpecMock);
+		when(requestHeadersSpecMock.retrieve())
+		        .thenReturn(responseSpecMock);
+		when(responseSpecMock.bodyToMono(ApiExampleWrapper.class))
+		        .thenReturn(resultMonoMock);
+		when(resultMonoMock.block())
+		        .thenReturn(expectedWrapper);
+
+		String expectedStory =
+                article.getTitle() + " -\n"
+                        + article.getContent()
+                        + "\nFull article: " + article.getUrl();
+        
         //when
+        String actualStory = newsApi.findStory(topic);
 
         //then
+        verify(webClientMock, times(1)).get();
+        assertEquals(expectedStory, actualStory);
     }
 
 
